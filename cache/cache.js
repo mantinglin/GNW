6f59b4fb45f15d1a31b2693b468902583567f139
JiaYi Zhang <bbfag3@gmail.com>
2020-06-18 15:44:23 -0700

    rua

---
+++ cache/cache.js
@@ -0,0 +1,3 @@
+module.exports = {
+    fakeCache: new Map()
+}
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
+var cache = require('./cache/cache')
+
 // var firebase = require("firebase/app");
 // var firestore = require("firebase/firestore");
 
@@ -18,25 +20,41 @@ var io = require('socket.io')(http);
 
 // firebase.firestore().collection()
 
+
+var serverId = 'msgServer1'
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
+function fakeAuth(jwt) {
+    return jwt
+}
 
 io.on('connection', (socket) => {
+    io.emit("newConn", "asddasadsasd")
+    //console.log("New connection: %s", socket)
     //{'target': conversationId, 'type': ['text', ...],'message': content}
     socket.on('chatMessage', (data) => {
-        io.emit('chat message', msg);
+        console.log(data)
+        io.to(data['cId']).emit('chatMessage',data['message'])
     });
 
     //{'token': jwtToken}
     socket.on('connection', (data) => {
-
+        console.log("New connection: %s",data)
+        //let uid = fakeAuth(data)
+        //cache.fakeCache[uid] = serverId
     })
 
-    //{'conversation': [conversationId], 'lastSeen': [messageIdOfLastSeen]}
+    //{'cIds': [conversationId], 'lastSeen': [messageIdOfLastSeen]}
     socket.on('fetchChat', (data) => {
-
+        console.log(data)
+        let cIds = data['cIds']
+        for(let c=0;c<cIds.length;c++) {
+            console.log("Joining %s", cIds[c])
+            socket.join(cIds[c])
+        }
     })
 });
 
