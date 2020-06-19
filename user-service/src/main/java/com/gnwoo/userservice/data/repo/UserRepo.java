150f7e60f5e8370a5c17c7b650cc0a77917edbcc
Xiaolei Luo <luo19980630@outlook.com>
2020-06-19 16:31:31 -0700

    Deleted files that do not belong to auth-service

--- user-service/src/main/java/com/gnwoo/userservice/data/repo/UserRepo.java
+++
@@ -1,8 +0,0 @@
-package com.gnwoo.userservice.data.repo;
-
-import com.gnwoo.userservice.data.table.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepo extends CrudRepository<User, Long> {
-
-}
